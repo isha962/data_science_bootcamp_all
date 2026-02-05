05433b21dd16c85aa0b95d362ae3ebd0ed7a7a85
isha962 <isha962hazel@gmail.com>
2026-02-05 17:35:41 -0500

    Squashed 'assignments/assignment_1/' content from commit a701f0c
    git-subtree-dir: assignments/assignment_1
    git-subtree-split: a701f0c116c4f1b89e66891ae5eb15e832e0a5f5

---
+++ vowel_count.py
@@ -0,0 +1,10 @@
+
+word = input("Enter a word: ")
+vowels = "aeiouAEIOU"
+count = 0
+
+for char in word:
+    if char in vowels:
+        count += 1
+
+print(f"The number of vowels in '{word}' is {count}.")
